db70e460bed60e7769c293d89dc0772c6d2bc30f
Pablo Gabriel <yellowtutors@gmail.com>
2026-07-17 13:14:00 -0300

    feat: Populate option 1

--- RTCRangeHelper.py
+++ RTCRangeHelper.py
@@ -4,7 +4,7 @@ from time import sleep
 exit = False
 while not exit:
     system("clear")
-    print("RTCRangeHelper\n")
+    print("\nRTCRangeHelper\n")
     print("1. Init the process")
     print("2. Exit")
     
@@ -13,8 +13,22 @@ while not exit:
     # 1. Init the process
     if option == "1":
         system("clear")
-        print("Init")
+        print("Make sure that you already test the DisableRtcChecksum quirk in Kernel -> Quirks")
+        sleep(3)
+        system("clear")
+        print("Let's get start with the 0x00-0xFF range to see if your CMOS problem is related to RTC ranges.")
         sleep(2)
+        print("\nPut this in your boot-args: ")
+        print("rtcfx_exclude=00-FF")
+        sleep(2)
+        print("\nMake sure to have RTCMemoryFixup.kext in your kexts")
+        print("https://github.com/acidanthera/RTCMemoryFixup/releases")
+        sleep(2)
+
+        print("\nNow reboot.")
+        exit_option = input("\nPress enter to exit...")
+        system("clear")
+        exit = True
     # 2. Exit
     elif option == "2":
         print("Exiting")
