81119d9ce537f64f8d94d4dce6b336cf63ecdc41
Pablo Gabriel <yellowtutors@gmail.com>
2026-07-22 12:59:06 -0300

    feat: Add "continue process" part

--- RTCRangeHelper.py
+++ RTCRangeHelper.py
@@ -22,6 +22,63 @@ except:
 exit = False
 while not exit:
     system("clear")
+
+    # If the process already started
+    if data["rangesExcluded"] == [[0, 255]] or len(data["rangesTried"]) >= 1:
+        print("\nRTCRangeHelper\n")
+        print("1. Continue the process")
+        print("2. Start over the process")
+        print("3. Exit")
+
+        option = input("\nChoose a option: ")
+
+        # 1. Continue the process
+        if option == "1":
+            errorQuestion = input("\nDid you still get the CMOS error? (y/n): ")
+            if errorQuestion.lower() == "y":
+                if data["rangesExcluded"] == [[0, 255]]:
+                    system("clear")
+                    print("You can try the 0x00-0x7F and 0x80-0xFF ranges together. The bit between these can be the RTC issue.")
+
+                    print("Change the range to this in your boot-args: ")
+                    print("\nrtcfx_exclude=00-7F,80-FF")
+
+                    with open("data.json", "w", encoding="utf-8") as f:
+                        data["rangesTried"].append(data["rangesExcluded"][0])
+                        data["rangesExcluded"] = [[0, 127], [128, 255]]
+                        json.dump(data, f, indent=4)
+                    
+                    sleep(2)
+                    print("\nNow reboot.")
+                    exit_option = input("\nPress enter to exit...")
+                    system("clear")
+                    exit = True
+            elif errorQuestion.lower() == "n":
+                system("clear")
+                print("We found the error. Let's mitigate!")
+                sleep(2)
+
+            else:
+                system("clear")
+                print("Invalid answer")
+                sleep(2)
+            continue
+        # 2. Start over the process
+        elif option == "2":
+            print("2")
+            sleep(2)
+            continue
+        # 3. Exit
+        elif option == "3":
+            print("Exiting")
+            exit = True
+            continue
+        else:
+            system("clear")
+            print("Invalid option")
+            sleep(2)
+            continue
+
     print("\nRTCRangeHelper\n")
     print("1. Init the process")
     print("2. Exit")
